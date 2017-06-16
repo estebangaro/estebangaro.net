af9e51b99fae666ef811034cc8c9d810ce0d74d6
Esteban GaRo <estebangaro@outlook.com>
2017-06-16 00:06:26 -0500

    Update Carrusel + Flechas desplazamiento
    Para concluir esta funcionalidad bloquear evts de flecha entre transiciones de contenidos.

--- estebangaro.net/Recursos/javascript/componente carrusel/carrusel.js
+++ estebangaro.net/Recursos/javascript/componente carrusel/carrusel.js
@@ -6,19 +6,63 @@ $(function() {
     enlazarEvts();
 });
 
-function obtenDesplazamientoHorizontal() {
-    return (_carruselconfiguracion.cantidadElementos - _carruselconfiguracion.indice) * -100;
+function obtenDesplazamientoHorizontal(indice) {
+    return (_carruselconfiguracion.cantidadElementos -
+        (indice != undefined ? indice :
+            _carruselconfiguracion.indice)) * -100;
+}
+
+function validaDesplazamientoInactivo(estado) {
+    var _inactivo = _carruselconfiguracion.activo == 'cont_1' ? 'cont_2' : 'cont_1';
+    if (estado) {
+        if (_carruselconfiguracion.indice == 0) {
+            configuraContenido(_inactivo, true, '100%', '0');
+        } else if (_carruselconfiguracion.indice == _carruselconfiguracion.cantidadElementos + 1) {
+            configuraContenido(_inactivo, true, (_carruselconfiguracion.cantidadElementos * -100) + '%',
+                obtenDesplazamientoHorizontal(1) + '%');
+        }
+        return true;
+    } else if (_carruselconfiguracion.indice == 0 ||
+        _carruselconfiguracion.indice == _carruselconfiguracion.cantidadElementos + 1) {
+        configuraContenido(_carruselconfiguracion.activo, false);
+        _carruselconfiguracion.activo = _inactivo;
+        _carruselconfiguracion.indice = _carruselconfiguracion.indice == 0 ?
+            _carruselconfiguracion.cantidadElementos : 1;
+    }
+}
+
+function configuraContenido(contenidoC, estado, desplazamiento, anima) {
+    if (estado) {
+        $('#' + contenidoC).css({
+            'left': desplazamiento, // obtenDesplazamientoHorizontal(_carruselconfiguracion.cantidadElementos + 1)
+            'display': 'block'
+        }).attr('class', 'contenidoCarrusel animaCarrusel');
+    } else
+        $('#' + contenidoC).css({
+            'display': 'none'
+        }).removeClass('animaCarrusel');
 }
 
 function desplazaCarrusel(manejadorFlecha) {
+    var _desplazaI;
+    var _inactivo = _carruselconfiguracion.activo == 'cont_1' ? 'cont_2' : 'cont_1';
     if (manejadorFlecha != undefined)
-        manejadorFlecha(); // Configuración y desplazamiento de contenedor espejo inactivo.
+        _desplazaI = manejadorFlecha(true); // Configuración y desplazamiento de contenedor espejo inactivo.
     var _posicion = obtenDesplazamientoHorizontal() + '%';
     $('#' + _carruselconfiguracion.activo).css({
         'left': _posicion
     });
+    if (_desplazaI) {
+        setTimeout(function() {
+            $('#' + _inactivo).css({
+                'left': _carruselconfiguracion.indice == 0 ? '0' : obtenDesplazamientoHorizontal(1) + '%'
+            });
+        }, 10);
+    }
     if (manejadorFlecha != undefined)
-        manejadorFlecha(true); // Actualización de contenedor espejo activo.
+        setTimeout(function() {
+            manejadorFlecha(false); // Actualización de contenedor espejo activo.
+        }, 1000);
 }
 
 function cargaAvisos() {
@@ -41,5 +85,9 @@ function enlazarEvts() {
     $('#contenidoBtns > img').click(function() {
         _carruselconfiguracion.indice = obtenPosicionBtn($(this));
         desplazaCarrusel();
+    });
+    $('#contenidoFlechas>img').click(function() {
+        _carruselconfiguracion.indice += obtenPosicionBtn($(this));
+        desplazaCarrusel(validaDesplazamientoInactivo);
     });
 }
