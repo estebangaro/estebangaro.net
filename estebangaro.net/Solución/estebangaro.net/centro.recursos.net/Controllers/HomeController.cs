9eee183fa8c97e1f9db9b7c2c8105f1afd79c977
Esteban GaRo <estebangaro@outlook.com>
2017-07-01 20:52:45 -0500

    Update Modelo Menú, Carrusel
    Se incluyen vistas parciales de ambos componentes, por definir vista principal para pruebas, además de añadir avisos de prueba en BD.

--- estebangaro.net/Solución/estebangaro.net/centro.recursos.net/Controllers/HomeController.cs
+++ estebangaro.net/Solución/estebangaro.net/centro.recursos.net/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using centro.recursos.net.Models.Entity_Framework;
+using centro.recursos.net.Models.Utileria;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +8,36 @@ using System.Web.Mvc;
 
 namespace centro.recursos.net.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : ControladorBase
     {
         // Home
-        public ActionResult GaRo()
+        public ActionResult GaRoNET()
         {
             throw new NotImplementedException();
+        }
+
+        public PartialViewResult _Menu()
+        {
+            Respuesta<List<OpcionMenu>> opciones = Repositorio.ObtenOpcionesMenu();
+            PartialViewResult resultado = null;
+            if (opciones.Estado)
+                resultado = PartialView(opciones.Resultado);
+            else
+                ViewBag.LayoutExcepcion = GeneraRespuestaExcepcion<List<OpcionMenu>>(opciones);
+
+            return resultado;
+        }
+
+        public PartialViewResult _Carrusel()
+        {
+            Respuesta<List<AvisoCarrusel>> avisos = Repositorio.ObtenAvisosCarrusel();
+            PartialViewResult resultado = null;
+            if (avisos.Estado)
+                resultado = PartialView(avisos.Resultado);
+            else
+                ViewBag.LayoutExcepcion = GeneraRespuestaExcepcion<List<AvisoCarrusel>>(avisos);
+
+            return resultado;
         }
     }
 }
---
+++ estebangaro.net/Solución/estebangaro.net/centro.recursos.net/Resources/javascript/home.js
@@ -0,0 +1,18 @@
+var _componenteUI_btnBusqueda = true; // Bandera de componente ui búsqueda.
+
+$(function() {
+    $(document).click(function() {
+        establecePredeterminado();
+    });
+    $(window).resize(function() {
+        $('*').attr('style', '');
+    });
+});
+
+function establecePredeterminado() {
+    ajustaBtnBusqueda(); // Determina la lógica de ajuste de componente ui búsqueda.
+}
+
+function ajustaAltoFondo() {
+
+}
