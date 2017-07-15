0d12b03c08918ece36ec436285f7608f903bde3c
Esteban GaRo <estebangaro@outlook.com>
2017-07-15 00:55:37 -0500

    Update Sección Noticias GRL

--- estebangaro.net/Recursos/javascript/componente noticias/noticias.js
+++ estebangaro.net/Recursos/javascript/componente noticias/noticias.js
@@ -1,4 +1,9 @@
 $(function() {
+    ajustaAltoCdrNoticias();
+    ajustaAnchoNoticias();
+});
+
+function ajustaAnchoNoticias() {
     // Margen derecho por bloque de noticia de 5px.
     // Ancho de icono de imagen por bloque de noticia de 60px.
     // Calculo de ancho por bloque = (anchoContenedor - (noBloques - 1) * margenDerecho) / noBloques
@@ -8,4 +13,11 @@ $(function() {
             'width': _ancho + 'px'
         });
     });
-});
+}
+
+function ajustaAltoCdrNoticias() {
+    var _alto = $('#contenedorMenu').innerHeight();
+    var _altoVentana = $(window).height();
+    var _altoSeccionNoticias = _altoVentana - _alto;
+    $('#seccion2Principal > .Contenido').css({ 'top': _alto + 'px', 'height': _altoSeccionNoticias + 'px' });
+}
