e0c664b4ff5bca93a70983048d53a0aa76ff0045
Esteban GaRo <estebangaro@outlook.com>
2017-08-29 23:11:43 -0500

    Update Convertidor de Codigo 2 HTML
    Se realiza prueba con clase controlador completa.

---
+++ estebangaro.net/Solución/estebangaro.net/centro.recursos.net/Models/Configuraciones Fluent API/PalabraCodigoConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+
+namespace centro.recursos.net.Models.Configuraciones_Fluent_API
+{
+    public class PalabraCodigoConfig: 
+        EntityTypeConfiguration<Entity_Framework.PalabraCodigo>
+    {
+        public PalabraCodigoConfig()
+        {
+            ToTable("PalabrasCodigo", "Configuracion");
+
+            Property(pc => pc.Nombre)
+                .HasColumnName("Descripcion")
+                .IsUnicode()
+                .HasMaxLength(30)
+                .IsRequired();
+
+            // Se define asociación con CategoriaPalabraCodigo.
+            HasRequired(pc => pc.Categoria)
+                .WithMany(cat => cat.Palabras)
+                .HasForeignKey(pc => pc.CategoriaId);
+        }
+    }
+}
---
+++ estebangaro.net/Solución/estebangaro.net/centro.recursos.net/Models/Entity Framework/CategoriaPalabraCodigo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace centro.recursos.net.Models.Entity_Framework
+{
+    public class CategoriaPalabraCodigo
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public InfoRegistro Auditoria { get; set; }
+
+        public virtual ICollection<PalabraCodigo> Palabras { get; set; }
+    }
+}
